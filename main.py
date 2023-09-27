3865874beb65c00a902909e2852ac4316af1e994
CHAMOMILE <xsmokx228@gmail.com>
2023-09-27 15:50:59 +0300

    add models

--- main.py
+++ main.py
@@ -8,14 +8,25 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-class TestModel(db.Model):
-    __tablename__ = 'test'
-
+class Reader(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    some = db.Column(db.String)
+    last_name = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(255), nullable=False)
+    patronymic = db.Column(db.String(255))
 
-    def __init__(self, some):
-        self.some = some
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+
+
+class Loan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    reader_id = db.Column(db.Integer, db.ForeignKey('reader.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    loan_date = db.Column(db.Date, nullable=False)
+    return_date = db.Column(db.Date)
 
 
 # main page
