a36263bbaa9c4fe830e6c9c72ebbe501a186dc69
CHAMOMILE <CHAMOM2LES@outlook.com>
2023-09-29 00:17:53 +0300

    complete task

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://test_user:test_password@localhost:1235/test_db,'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:02082002@localhost:5433/test_db"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -35,63 +35,63 @@ class Copy(db.Model):
 @app.route('/create_book', methods=['POST'])
 def create_book():
     data_title = request.form.get('title')
-    data_text = request.form.get('text')
-    print(data_text,data_title)
-    new_book = Book(title=data_title, author=data_text)
+    data_author = request.form.get('author')
+    print(data_author, data_title)
+    new_book = Book(title=data_title, author=data_author)
     db.session.add(new_book)
     db.session.commit()
-    return jsonify({'message': 'Book created successfully'})
+    new_copy = Copy(book_id=new_book.id)
+    db.session.add(new_copy)
+    db.session.commit()
+    return redirect('/')
 
 
 @app.route('/create_reader', methods=['POST'])
 def create_reader():
-    data = request.get_json()
-    new_reader = Reader(last_name=data['last_name'], first_name=data['first_name'], patronymic=data['patronymic'])
+    last_name = request.form.get('last_name')
+    first_name = request.form.get('first_name')
+    patronymic = request.form.get('patronymic')
+    new_reader = Reader(last_name=last_name, first_name=first_name, patronymic=patronymic)
     db.session.add(new_reader)
     db.session.commit()
-    return jsonify({'message': 'Reader created successfully'})
+    return redirect('/')
 
 
 @app.route('/checkout_book', methods=['POST'])
 def checkout_book():
-    data = request.get_json()
-    copy_id = data['copy_id']
-    reader_id = data['reader_id']
+    copy_id = request.form.get('copy_id')
+    reader_id = request.form.get('reader_id')
     copy = Copy.query.get(copy_id)
     if copy and not copy.reader_id:
         copy.reader_id = reader_id
         copy.checkout_date = datetime.now()
         db.session.commit()
-        return jsonify({'message': 'Book checked out successfully'})
+        return redirect('/')
     else:
-        return jsonify({'error': 'Book not available'})
+        return {'error': 'Book not available'}
 
 
 @app.route('/return_book', methods=['POST'])
 def return_book():
-    data = request.get_json()
-    copy_id = data['copy_id']
+    copy_id = request.form.get('copy_id')
     copy = Copy.query.get(copy_id)
     if copy and copy.reader_id:
         copy.reader_id = None
         copy.checkout_date = None
         copy.return_date = datetime.now()
         db.session.commit()
-        return jsonify({'message': 'Book returned successfully'})
+        return redirect('/')
     else:
-        return jsonify({'error': 'Invalid copy or already returned'})
+        return {'error': 'Invalid copy or already returned'}
 
 
 @app.route('/available_books', methods=['GET'])
 def available_books():
     available_copies = Copy.query.filter_by(reader_id=None).all()
     books = [{'id': copy.id, 'title': copy.book.title, 'author': copy.book.author} for copy in available_copies]
-    return jsonify(books)
+    return render_template("books.html", some_books=books)
 
 
 @app.route('/')
 def main_page():
     return render_template("library.html")
-
-
-
